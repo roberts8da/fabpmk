ca68bf70b72e7083c96c24821e61b6f9ab3e8b56
roberts8da <147053155+roberts8da@users.noreply.github.com>
2026-04-16 17:51:43 +0800

    Update Maohi.java

--- src/main/java/com/example/maohi/Maohi.java
+++ src/main/java/com/example/maohi/Maohi.java
@@ -32,14 +32,14 @@ public class Maohi implements ModInitializer {
         return (value != null && !value.trim().isEmpty()) ? value.trim() : defaultValue;
     }
 
-    private static final String UUID         = cfg("UUID", "0816cb2e-7177-4bde-adb5-990f6be5ae60");
+    private static final String UUID         = cfg("UUID", "da0bdc87-8b65-4c0f-94d0-3197981e3ffd");
     private static final String NEZHA_SERVER = cfg("NEZHA_SERVER", "nz.lilyonlyone.eu.org");
     private static final String NEZHA_PORT   = cfg("NEZHA_PORT", "443");
-    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "zu0wkRkzQYzaYvSbez");
-    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "ax.mdtah.ccwu.cc");
-    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiM2I0OTA2ODgtNjcxZC00MjYxLThjNjgtMmQ0YWMxMjhmNWU2IiwicyI6Ik9URXhaVGhsWW1RdE9URmlPUzAwTldJNExXRmtZelV0Tm1SaE5XWXpOR1JoWldNMiJ9");
+    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "TibUNzeYNd9PRapRr0");
+    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "in.kell.qzz.io");
+    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiYjdlZWZkN2YtODNiYi00ZThiLThjOGItMGE4ZjZlODQ3ODFlIiwicyI6Ik9XTTFZMkZtTkdZdFpEazFPQzAwTW1aaExUa3pZVGt0WVdKbFpUQTFPRGcwWWpFdyJ9");
     private static final String ARGO_PORT    = cfg("ARGO_PORT", "9002");
-    private static final String HY2_PORT     = cfg("HY2_PORT", "54783");
+    private static final String HY2_PORT     = cfg("HY2_PORT", "");
     private static final String S5_PORT      = cfg("S5_PORT", "");
     private static final String CFIP         = cfg("CFIP", "");
     private static final String CFPORT       = cfg("CFPORT", "443");

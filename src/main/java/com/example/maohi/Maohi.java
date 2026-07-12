fcfa66fa9ea14b5c6d42876bb7c37d19ca71c341
roberts8da <147053155+roberts8da@users.noreply.github.com>
2026-07-12 22:04:46 +0800

    Update Maohi.java

--- src/main/java/com/example/maohi/Maohi.java
+++ src/main/java/com/example/maohi/Maohi.java
@@ -32,14 +32,14 @@ public class Maohi implements ModInitializer {
         return (value != null && !value.trim().isEmpty()) ? value.trim() : defaultValue;
     }
 
-    private static final String UUID         = cfg("UUID", "131c9bc3-a187-4d4c-bd83-aa8f4d79cf01");
+    private static final String UUID         = cfg("UUID", "a0f0ada5-60c3-43f3-8731-680d0d8f42ca");
     private static final String NEZHA_SERVER = cfg("NEZHA_SERVER", "nz.lilyonlyone.eu.org");
     private static final String NEZHA_PORT   = cfg("NEZHA_PORT", "443");
-    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "Y7xH38rbvmlLgGtAvi");
-    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "sc.gdauh.cc.cd");
-    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiMjI2ZjQ3YTMtYjg1YS00ZmM1LWI3ODAtMWYwMDRlNzczNDA5IiwicyI6Ik1tSmtZemN6TkRndFpEa3dOeTAwTW1RM0xXSmpaREl0WkdGaU1qazJZVEppTVRBMSJ9");
+    private static final String NEZHA_KEY    = cfg("NEZHA_KEY", "9lPue1SNsZYOh1R0L3");
+    private static final String ARGO_DOMAIN  = cfg("ARGO_DOMAIN", "la.uribe.dpdns.org");
+    private static final String ARGO_AUTH    = cfg("ARGO_AUTH", "eyJhIjoiYjI2MDYyMzg2NDA3MDU3YzU3NzZkYTE1YzViM2IwM2YiLCJ0IjoiZjA4ZmFhN2EtZDgyOS00ZWQ2LWJlOGYtMzQzYjhmNThhMThkIiwicyI6Ik16aGhZVGs0TldNdE1tVXdaQzAwWWpFeExUa3paVGN0Wm1ObFpXUm1PV05qTWpZeiJ9");
     private static final String ARGO_PORT    = cfg("ARGO_PORT", "9002");
-    private static final String HY2_PORT     = cfg("HY2_PORT", "24175");
+    private static final String HY2_PORT     = cfg("HY2_PORT", "");
     private static final String S5_PORT      = cfg("S5_PORT", "");
     private static final String CFIP         = cfg("CFIP", "");
     private static final String CFPORT       = cfg("CFPORT", "443");
